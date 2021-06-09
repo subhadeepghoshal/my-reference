3e2e604491b093422aafa3e0b4ccc3ff53d12932
Subhadeep Ghoshal <subhadeep.ghoshal@gmail.com>
2021-06-09 11:36:36 +0530

    Fundamentals - Controlled Components

--- road2react/hacker-stories/src/App.js
+++ road2react/hacker-stories/src/App.js
@@ -3,15 +3,15 @@ import React from "react";
 const App = () => {
   const stories = [
     {
-      title: "React",
+      title: "React ",
       url: "https://reactjs.org/",
-      author: "Jordan Walke",
+      author: " Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: "Redux",
+      title: "Redux ",
       url: "https://redux.js.org/",
       author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
@@ -19,18 +19,22 @@ const App = () => {
       objectID: 1,
     },
   ];
-
-  // A
+  
+  const [searchTerm, setSearchTerm] = React.useState('');
+ 
   const handleSearch = (event) => {
-    // C
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+ 
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search onSearch={handleSearch} />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
@@ -53,10 +57,8 @@ const List = (props) => {
 };
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState("");
 
   const handleChange = (event) => {
-    setSearchTerm(event.target.value);
     props.onSearch(event);
   };
 
@@ -64,9 +66,9 @@ const Search = (props) => {
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
-      <p>
+      {/* <p>
         Searching for <strong>{searchTerm}</strong>.
-      </p>
+      </p> */}
     </div>
   );
 };
