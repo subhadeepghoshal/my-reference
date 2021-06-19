b0786c9679d3cb51ab76115df8a928bf9bfddb67
Subhadeep Ghoshal <subhadeep.ghoshal@gmail.com>
2021-06-19 21:50:26 +0530

    odyssey lift off part 1 complete

--- graphql-odyssey/odyssey-lift-off-part1/client/src/pages/tracks.js
+++ graphql-odyssey/odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResult from '../components/query-result';
+
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  return (
+    <Layout grid>
+<QueryResult error={error} loading={loading} data={data}>
+  {data?.tracksForHome?.map(track => (
+    <TrackCard key={track.id} track={track} />
+  ))}
+</QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
