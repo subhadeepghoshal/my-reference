d89e697752b57b30059ee099d25fe8ae60bac5af
Subhadeep Ghoshal <subhadeep.ghoshal@gmail.com>
2021-07-01 19:39:20 +0530

    Road2React Fundamentals - Props Handling. Complete
    Props Handling with Object De-structuring, Rest and Spread operator. Although Book reverted back the changes to avoid advanced javascript techniques, kept it as these reduces clutter

--- road2react/hacker-stories/src/App.js
+++ road2react/hacker-stories/src/App.js
@@ -41,32 +41,32 @@ const App = () => {
 
 const List = ({ list }) => (
   <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+    {list.map(({objectId, ...item}) => (
+      <Item key={objectId} {...item} />
     ))}
   </ul>
 );
 
-const Item = ({ item }) => (
+const Item = ({url,title,author,num_comments,points }) => (
   <li>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 );
 
-const Search = (props) => {
+const Search = ({search, onSearch}) => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input
         id="search"
         type="text"
-        value={props.search}
-        onChange={props.onSearch}
+        value={search}
+        onChange={onSearch}
       />
       {/* <p>
         Searching for <strong>{searchTerm}</strong>.
