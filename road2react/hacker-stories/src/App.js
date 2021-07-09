0f9be3f8197cb9568c80db46e06d5f7cf1ad0caf
Subhadeep Ghoshal <subhadeep.ghoshal@gmail.com>
2021-07-09 11:38:33 +0530

    Road2React Fundamentals - Remote Data Fetching using API
    Local data fetch (initialStories array and getAsyncStories function)  replaced with fetching data using Rest API inside useEffect hook

--- road2react/hacker-stories/src/App.js
+++ road2react/hacker-stories/src/App.js
@@ -1,31 +1,7 @@
 import React from "react";
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
-  /***  Stories Data ***/
-  const initialStories = [
-    {
-      title: "React ",
-      url: "https://reactjs.org/",
-      author: " Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux ",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  /***  Simulator function for fetching stories from a remote store ***/
-  const getAsyncStories = () =>
-    new Promise((resolve) =>
-      setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-    );
 
   /***  Custom Hook extending useState, manages state syncronyzed with local storage, used for SerachTerm ***/
   const useSemiPersistentState = (key, initialState) => {
@@ -90,11 +66,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`) // B
+      .then((response) => response.json()) // C
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_INIT" }));
@@ -165,8 +142,8 @@ const Item = ({
     <span>
       <a href={url}>{title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
+    <span> {author} </span>
+    <span>{num_comments}  </span>
     <span>{points} </span>
     <span>
       <button type="button" onClick={() => onRemoveItem(objectID)}>
